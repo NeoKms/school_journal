bbe61788553613e10c75021eb0f4837846412e0e
Jiuglskiy.Vladislav <Jiuglskiy.Vladislav@chtp.net>
2020-10-17 19:14:44 +0300

    fix bug

--- classes/sentry.php
+++ classes/sentry.php
@@ -1,7 +1,7 @@
 <?php
 if (file_exists(ROOT . 'env.php')) {
-    include_once(ROOT . '/env.php');
-    include_once(ROOT . '/classes/vendor/autoload.php');
+    include_once(ROOT . 'env.php');
+    include_once(ROOT . 'classes/vendor/autoload.php');
     if (function_exists('Sentry\init')) {
         Sentry\init(['dsn' => $sentryDsn]);
         define('SENTRY_EXISTS',true);
